9b5dcacce4ba61c9806e7035e64e339c6f92fe11
Cat <pozdyshev.vladimir@gmail.com>
2018-02-20 12:58:37 +0300

    mobile menu

--- _src/js/modules/main.js
+++ _src/js/modules/main.js
@@ -1,3 +1,22 @@
+//popups
 (function(){
+    function initPopup(prop){
+        var $body = $('body');
+        var $btnShow = prop.btnShow;
+        var $btnHide = prop.btnHide;
+        $btnShow.on('click', function(e){
+            e.preventDefault();
+            $body.addClass(prop.openClass);
+        });
+        $btnHide.on('click', function(e){
+            e.preventDefault();
+            $body.removeClass(prop.openClass);
+        });
+    }
 
+    initPopup({
+        btnShow: $('.header__burger'),
+        btnHide: $('.mobile_menu__cross'),
+        openClass: 'openedMenu'
+    });
 })();
